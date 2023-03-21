c383d731498bc312fa1e9b252c612758ed48561d
skybluelion <brotherjun96@gmail.com>
2023-03-22 08:54:45 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 124 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		
+		int[] arr = new int[4];
+		arr[0] = Integer.parseInt(st.nextToken());
+		arr[1] = Integer.parseInt(st.nextToken());
+		int w = Integer.parseInt(st.nextToken());
+		int h = Integer.parseInt(st.nextToken());
+		arr[2] = w - arr[0];
+		arr[3] = h - arr[1];
+		
+		Arrays.sort(arr);
+		
+		
+		bw.write(arr[0]+"");
+		
+		bw.flush();
+		bw.close();
+		br.close();
+
+	}
+}
