4102225d22315e822ad7a8ca5c2e0bd454787032
skybluelion <brotherjun96@gmail.com>
2023-03-22 00:11:11 +0900

    [Bronze I] Title: 소인수분해, Time: 192 ms, Memory: 16908 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));	
+
+		int N = Integer.parseInt(br.readLine());
+		int i = 2;
+        while (N != 1) {
+            if (N % i == 0) {
+                N/= i;
+                bw.write(i + "\n");
+            } else i++;
+        }
+
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
