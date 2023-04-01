cea7571422f8788665b8233f1358eb3be5cde3a6
skybluelion <brotherjun96@gmail.com>
2023-04-01 23:32:15 +0900

    [Bronze II] Title: 진법 변환, Time: 132 ms, Memory: 14380 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String num = st.nextToken();
+		int B = Integer.parseInt(st.nextToken());
+		int idx = num.length();
+		int su = 0 ;
+		for (int i = 0; i < num.length(); i++) {
+			idx--;
+			if(num.charAt(i)-'0'<10) su += Math.pow(B,idx)*(num.charAt(i)-'0');
+			else su += Math.pow(B,idx)*(num.charAt(i)-'7');
+		}
+		bw.write(su+"");
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
