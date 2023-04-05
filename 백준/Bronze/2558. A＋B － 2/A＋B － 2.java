03d135cb13ba64b9fdb7ce5d3f3ec76c41aaee1c
skybluelion <brotherjun96@gmail.com>
2023-04-05 17:28:37 +0900

    [Bronze V] Title: A+B - 2, Time: 128 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+		
+		bw.write(A+B+"");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+	
+}
