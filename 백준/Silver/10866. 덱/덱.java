80f664bafc18829f414ec8093000cbd5faa82d7e
skybluelion <brotherjun96@gmail.com>
2023-04-15 19:53:34 +0900

    [Silver IV] Title: 덱, Time: 252 ms, Memory: 19836 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(st.nextToken());
+		Deque<Integer> q = new LinkedList<>();
+		
+		for (int i = 0; i < N; i++) {
+	    	st = new StringTokenizer(br.readLine());
+	    	String str = st.nextToken();
+	    	switch(str) {
+	    	case "push_front":
+	    		q.addFirst(Integer.parseInt(st.nextToken()));
+	    		break;
+	    	case "push_back":
+	    		q.addLast(Integer.parseInt(st.nextToken()));
+	    		break;
+	    	case "pop_front":
+    			if(q.isEmpty()) bw.write(-1+"\n");
+	    		else {
+		    		int A = q.pollFirst();
+    				bw.write(A+"\n");	
+    			}
+    			break;
+	    	case "pop_back":
+	    		if(q.isEmpty()) bw.write(-1+"\n");
+	    		else {
+	    			int A = q.pollLast();
+	    			bw.write(A+"\n");	
+	    		}
+	    		break;
+    		case "size":
+    			bw.write(q.size()+"\n"); 
+    			break;
+    		case "empty":
+    			if(q.isEmpty()) bw.write(1+"\n");
+	    		else bw.write(0+"\n");
+		    	break;
+       		case "front":
+	    		if(q.isEmpty()) bw.write(-1+"\n");
+		    	else bw.write(q.peek()+"\n");
+	    		break; 
+	    	default:
+	    		if(q.isEmpty()) bw.write(-1+"\n");
+	    		else bw.write(q.peekLast()+"\n");
+	    		break;
+		    }
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
