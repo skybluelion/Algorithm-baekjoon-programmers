9abd52cafd655031914e1512d0c8c6229a9f09e8
skybluelion <brotherjun96@gmail.com>
2023-05-08 12:54:55 +0900

    [Bronze V] Title: 큰 수 (BIG), Time: 292 ms, Memory: 20516 KB -BaekjoonHub

---
+++ 백준/Bronze/14928. 큰 수 （BIG）/큰 수 （BIG）.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String N = br.readLine();
+        long remain = 0;
+        for(int i =0; i < N.length(); i++){
+            remain = (remain * 10 + (N.charAt(i) - '0')) % 20000303;
+        }
+        bw.write(remain + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
