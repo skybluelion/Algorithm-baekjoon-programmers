6a8c5da7818509aa565cf8c4c08a2f100b1178b4
skybluelion <brotherjun96@gmail.com>
2023-05-23 08:50:55 +0900

    [Bronze V] Title: Copier, Time: 156 ms, Memory: 15852 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			int A  = Integer.parseInt(br.readLine());
+			System.out.println(A+" "+A);
+		}
+		
+		br.close();
+
+	}
+
+}
