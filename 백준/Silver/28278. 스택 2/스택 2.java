87d3615c81da0e28372b8450e65eedbcef86707f
skybluelion <brotherjun96@gmail.com>
2023-09-19 19:39:16 +0900

    [Silver IV] Title: 스택 2, Time: 760 ms, Memory: 130412 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+
+    static Stack<Integer> stack = new Stack<>();
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine()); //명령어의 수
+
+        while(N --> 0){
+            solution(br.readLine());
+        }
+        
+        System.out.println(sb);
+    }
+
+    static void solution(String query){
+        char c = query.charAt(0); //첫번째 문자는 명령어
+
+        switch (c){
+            case '1' : 
+                stack.push(Integer.parseInt(query.substring(2))); 
+                return;
+            case '2' : 
+                sb.append(stack.isEmpty() ? -1 : stack.pop()).append("\n"); 
+                return;
+            case '3' : 
+                sb.append(stack.size()).append("\n"); 
+                return;
+            case '4' : 
+                sb.append(stack.isEmpty() ? 1 : 0).append("\n"); 
+                return;
+            case '5' : 
+                sb.append(stack.isEmpty() ? -1 : stack.peek()).append("\n"); 
+                return;
+            default : 
+                break;
+        }
+    }
+}
