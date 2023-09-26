9aac47bc5d83e42495e6cdc48ab5b2afd168acc8
skybluelion <brotherjun96@gmail.com>
2023-09-26 12:22:58 +0900

    [Bronze II] Title: 거스름돈, Time: 128 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/5585. 거스름돈/거스름돈.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = 1000 - Integer.parseInt(br.readLine());
+		
+		int[] arr = {500, 100, 50, 10, 5, 1};
+		int answer = 0;
+		
+		for(int i = 0; i < arr.length; i++) {
+			answer += n / arr[i];
+			n = n % arr[i];
+		}
+		
+		System.out.println(answer);
+	}
+}
