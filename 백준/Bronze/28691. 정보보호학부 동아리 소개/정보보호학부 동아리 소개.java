aa7e48a3d245123c0ac2dfccc5cadcafea5c6b90
skybluelion <brotherjun96@gmail.com>
2023-11-12 14:19:07 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 120 ms, Memory: 14064 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		
+		switch (str) {
+		case "M": {
+			System.out.println("MatKor");
+			break;
+		}
+		case "W": {
+			System.out.println("WiCys");		
+			break;
+		}
+		case "C": {
+			System.out.println("CyKor");	
+			break;
+		}
+		case "A": {
+			System.out.println("AlKor");	
+			break;
+		}
+		case "$": {
+			System.out.println("$clear");	
+			break;
+		}
+
+		
+	}
+	}
+
+}
