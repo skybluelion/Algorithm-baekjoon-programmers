0668b61c37e697833af90c3553d2042d0622f58c
skybluelion <brotherjun96@gmail.com>
2023-11-19 15:38:07 +0900

    [level 2] Title: 구명보트, Time: 19.16 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -4,24 +4,30 @@ class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
         Arrays.sort(people);
-        
+        // 앞 뒤로 무게의 합을 비교해서 빼기 위해 Deque 사용
         Deque<Integer> dq = new LinkedList<>();
         for(int i = people.length - 1; i >= 0; i--){
             dq.add(people[i]);
         }
         int temp = 0;
+        
         while(true){
             if(dq.size() == 1){
                 dq.poll();
                 answer++;
                 break;
             }else{
+                // 정렬 시켜놓고 가장 큰 값부터 차례로 구출
+                // 가장 큰 값과 작은 값을 더하여 limit 보다 낮거나 같을 때만 작은 값 구출
                 temp = dq.pollFirst();
-                if(limit >= temp + dq.peekLast()){
-                    temp = dq.pollLast();
-                }
                 answer++;
-                if(dq.isEmpty()) break;
+                if(limit - temp < 40) continue;
+                else {
+                    if(limit >= temp + dq.peekLast()){
+                        temp = dq.pollLast();
+                    }
+                    if(dq.isEmpty()) break; 
+                }             
             }
         }
         return answer;
