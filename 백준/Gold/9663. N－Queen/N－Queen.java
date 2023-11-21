8f802415fac291f964c842c2858938e3f2a36cd3
skybluelion <brotherjun96@gmail.com>
2023-11-21 19:24:08 +0900

    [Gold IV] Title: N-Queen, Time: 3892 ms, Memory: 15104 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,53 +1,51 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
 
 public class Main {
 
-	static int N,cnt ;
-	static int[] columns = new int[N];  
+    static int N;
+    static int[] locations;
+    static boolean[] isVisited;
+    static int cnt = 0;
 
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		N = Integer.parseInt(st.nextToken());
-		columns = new int[N];// index번째 줄에 배치할 퀸의 위치
+    //1~N 까지의 수를 배치하고 대각선에 없는 경우만 따짐.
+    //백트래킹으로 나열
+    //대각선에 있는지 판단
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		setRowQueen(0); 
-		bw.write(cnt+"");
-		bw.flush();
-		bw.close();
-		br.close();
-	}
-	private static void setRowQueen(int line) {  //cnt번째 줄에 퀸배치
-		//기저조건
-		if(line ==N) {
-			cnt++;
-			return;
-		}
-		
-		
-		for (int i = 0; i < N; i++) {
-			columns[line]=i;		//cnt번째 줄에 i번째 위치에 퀸 배치 // 현재line까지 가능한지 판단(백트랙킹)
-			if(isAvailable(line)) setRowQueen(line+1); //다음줄에 퀸 배치
-		}
-		
+        N = Integer.parseInt(br.readLine());
 
-	}
-	
-	private static boolean isAvailable(int line) { // line 번째 놓여진 Queen의 타당성 판단 
-		// 같은 열에 있거나 대각선상에 있다면 불가능
-		for (int i = 0; i < line; i++) {
-			if(columns[line] == columns[i] || 
-					(line-i) == Math.abs(columns[line] -columns[i]))
-			return false;
-		}
-		return true;
-	}
+        for(int i = 0; i < N; i++){
+            locations = new int[N];
+            isVisited = new boolean[N];
+            backtracking(i, 0);
+        }
+
+        System.out.print(cnt);
+    }
+
+    public static void backtracking(int loca, int depth){
+        locations[depth] = loca;
+        isVisited[loca] = true;
+        if(depth == N - 1){
+
+            cnt++;
+            return;
+        }
+        for(int i = 0; i < N; i++){
+            if(!isVisited[i] && !isLine(i, depth + 1)) {
+                backtracking(i, depth + 1);
+                isVisited[i] = false;
+            }
+        }
+    }
+
+    public static boolean isLine(int x, int index){
+        for(int i = 0; i < index; i++){
+            if(Math.abs(locations[i] - x) == Math.abs(i - index)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
