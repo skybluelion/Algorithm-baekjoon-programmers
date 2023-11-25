eed91ae8ade490c04202f69d1d824598577df411
skybluelion <brotherjun96@gmail.com>
2023-11-25 10:03:01 +0900

    [Silver III] Title: 이친수, Time: 124 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main{
+    public static void main(String args[]) throws IOException{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int cnt = Integer.parseInt(br.readLine());
+            long dp[] = new long[cnt + 1];
+
+            dp[0] = 0;
+            dp[1] = 1;
+            for (int i = 2; i <= cnt; i++){
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+            System.out.println(dp[cnt]);
+    }
+}
