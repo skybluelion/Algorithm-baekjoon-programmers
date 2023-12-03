fd753a6812c75b81ed85c1fa38cba8f2c7a22393
skybluelion <brotherjun96@gmail.com>
2023-12-03 19:35:19 +0900

    [Gold IV] Title: 스도쿠, Time: 420 ms, Memory: 21136 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -1,96 +1,74 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Iterator;
-import java.util.StringTokenizer;
-
-// 백트랙킹 문제 dfs
-// 백준 15652 https://www.acmicpc.net/problem/15652
-
+import java.awt.*;
+import java.io.*;
+import java.util.*;
+import java.util.List;
 
 public class Main {
 
-	static int[][] arr = new int[9][9];
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[][] sudoku = new int[9][9];
+    static List<Point> pointList = new ArrayList<>();
 
-		for(int i = 0; i < 9; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			for(int j = 0; j < 9; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		sol(0,0);
-		
-	}
-	public static void sol(int row,int col) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-		
-		//열이 모두 채워졌을 경우
-		if(col == 9) {
-			sol(row + 1, 0);
-			return;
-		}
-		
-		// 행이 모두 채워졌을 경우
-		if (row == 9) {
-			for (int i = 0; i < 9; i++) {
-				for (int j = 0; j < 9; j++) {
-					System.out.print(arr[i][j] + " ");
-				}
-				System.out.println();
-			}
- 
-			// 출력 뒤 시스템을 종료
-			System.exit(0);
-		}
- 
-		// 1부터 9까지 중 가능한 수 탐색
-		if (arr[row][col] == 0) {
-			for (int i = 1; i <= 9; i++) {
-				// i값 중복검사
-				if (possibility(row, col, i)) {
-					arr[row][col] = i;
-					sol(row, col + 1);
-				}
-			}
-			arr[row][col] = 0;
-			return;
-		}
- 
-		sol(row, col + 1);
-		
-	}
-	
-	public static boolean possibility(int row, int col, int value) {
-		 
+        for (int i = 0; i < 9; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 9; j++) {
+                int token = Integer.parseInt(st.nextToken());
+                if (token == 0) pointList.add(new Point(i, j));
+                sudoku[i][j] = token;
+            }
+        }
 
-		for (int i = 0; i < 9; i++) {
-			// 같은 행 검사
-			if (arr[row][i] == value) return false;
-			
-			// 같은 열 검사
-			if (arr[i][col] == value) return false;
-		}
+        dfs(0);
 
- 
-		// 3*3 칸 중복 검사
-		int set_row = (row / 3) * 3; // value가 속한 3x3의 행의 첫 위치
-		int set_col = (col / 3) * 3; // value가 속한 3x3의 열의 첫 위치
- 
-		for (int i = set_row; i < set_row + 3; i++) {
-			for (int j = set_col; j < set_col + 3; j++) {
-				if (arr[i][j] == value) {
-					return false;
-				}
-			}
-		}
- 
-		return true; // 중복되는 것이 없을 경우 true 반환
-	}
-	
+    }
+
+    static void dfs(int index) throws IOException {
+        if (index == pointList.size()) {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    sb.append(sudoku[i][j]).append(" ");
+                }
+                sb.append("\n");
+            }
+            System.out.print(sb);
+            System.exit(0);
+
+        }
+
+        int x = pointList.get(index).x;
+        int y = pointList.get(index).y;
+
+        for (int i = 1; i <= 9; i++) {
+            if (lineChecked(x, y, i) && boxChecked(x, y, i)) {
+                sudoku[x][y] = i;
+                dfs(index + 1);
+                sudoku[x][y] = 0;
+            }
+
+        }
+    }
+
+    static boolean lineChecked(int row, int col, int value) {
+        for (int i = 0; i < 9; i++) {
+            if (sudoku[row][i] == value || sudoku[i][col] == value) return false;
+        }
+        return true;
+    }
+
+    static boolean boxChecked(int row, int col, int value) {
+        int x = (row / 3) * 3;
+        int y = (col / 3) * 3;
+
+        for (int i = x; i < x + 3; i++) {
+            for (int j =  y; j< y + 3; j++) {
+                if (sudoku[i][j] == value) return false;
+            }
+        }
+        return true;
+    }
 }
