25e06bc9c945bc7793ddcb3386a3f350877d380f
skybluelion <brotherjun96@gmail.com>
2023-12-04 23:16:43 +0900

    [level 3] Title: 등굣길, Time: 0.96 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,30 +1,39 @@
 class Solution {
+    
+    static int[][] map;
+    static boolean[][] isBlocked;
+    
     public int solution(int m, int n, int[][] puddles) {
-        int answer = 0;
-        
-        int[][] map = new int[n + 1][m + 1];
-        boolean[][] isBlocked = new boolean[n + 1][m + 1];
+
+        map = new int[n + 1][m + 1];
+        isBlocked = new boolean[n + 1][m + 1];
         
         for(int[] puddle : puddles){
             isBlocked[puddle[1]][puddle[0]] = true;
         }
-        
+
+        return dp(n, m, 1000000007);
+    }
+    
+    public static int dp(int n, int m, int x){
         map[1][1] = 1;
+        
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
                 if(!isBlocked[i][j]){
                     if(map[i - 1][j] > 0) {
-                        map[i][j] += map[i - 1][j] % 1000000007;
+                        map[i][j] += map[i - 1][j] % x;
                     }
                     if(map[i][j - 1] > 0) {
-                        map[i][j] += map[i][j - 1] % 1000000007;
+                        map[i][j] += map[i][j - 1] % x;
                     }
                 }
             }
-        }
+        }  
         
-        answer = map[n][m] % 1000000007;
+        int answer = map[n][m] % x;
         
         return answer;
     }
+
 }
