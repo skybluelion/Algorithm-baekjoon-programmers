f3edea4c8f36ce15aa59f8211a544ae9ef43dd7b
HyungJun <brotherjun96@gmail.com>
2024-01-07 13:57:20 +0900

    [level 2] Title: 배달, Time: 1.13 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -0,0 +1,58 @@
+/**
+ https://school.programmers.co.kr/learn/courses/30/lessons/12978
+ 배달 - Summer/Winter Coding(~2018)
+
+ - DFS 깊이 우선 탐색
+
+ **/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Solution {
+    static int answer;
+    static int K;
+    static ArrayList<ArrayList<int[]>> list = new ArrayList<>();
+    static int[] time; // 각 마을에 걸리는 시간
+
+
+    public int solution(int N, int[][] road, int K) {
+        answer = 1;
+        this.K = K;
+
+        // 1. 최댓값으로 초기화
+        time = new int[N+1];
+        Arrays.fill(time, Integer.MAX_VALUE);
+
+        // 2. 인접 list road 기반 초기화
+        for(int i = 0; i < N + 1; i++){
+            list.add(new ArrayList<>());
+        }
+        for (int[] a : road) {
+            list.get(a[0]).add(new int[]{a[1], a[2]});
+            list.get(a[1]).add(new int[]{a[0], a[2]});
+        }
+
+        // 3. dfs
+        dfs(1, 0);
+
+        return answer;
+    }
+
+    public void dfs(int n, int weight){
+        // 지금까지의 시간
+        time[n] = weight;
+
+        for(int[] node : list.get(n)){
+            // 현재 시간에 다음 마을을 가는 시간을 더한 값
+            int sum = node[1] + weight;
+
+            // 1. sum이 만약 K를 넘어가거나 node[0]번 마을의 지금까지의 최솟값보다 크다면 진행
+            if(K < sum || time[node[0]] < sum) continue;
+            // 2. 방문하지 않은 경우
+            if(time[node[0]] == Integer.MAX_VALUE) answer++; // 배달이 가능하기 때문에 +1
+
+            dfs(node[0], node[1] + weight);
+        }
+    }
+}
