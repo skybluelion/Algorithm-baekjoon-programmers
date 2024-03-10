a4200c9604d5c2fc01dc588a8b415d14d4c08e29
HyungJun <brotherjun96@gmail.com>
2024-03-10 15:10:11 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,4 @@
+SELECT animal_id, name, sex_upon_intake
+FROM animal_ins
+WHERE name = 'Lucy' or name = 'Ella' or name = 'Pickle' or name = 'Rogan' or name = 'Sabrina' or name = 'Mitty'
+ORDER BY animal_id
