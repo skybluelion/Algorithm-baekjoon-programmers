94bceed9ab3cba14191797e6eee0f7bb2064cec6
HyungJun <brotherjun96@gmail.com>
2024-03-24 15:12:08 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 82.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n, int a, int b) {
+        int cnt = 0;
+        
+        for(int i = n / 2; i >= 0; i--){
+            if(a == b) break;
+            
+            if(a % 2 == 1) a += 1;
+            a /= 2;
+            
+            if(b % 2 == 1) b += 1;
+            b /= 2;
+            
+            cnt++;
+        }
+        return cnt;
+    }
+}
